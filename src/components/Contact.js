23e68f10fcd495ea65050f0e49aff94bf00e7e0b
unknown <you@example.com>
2021-08-11 21:23:45 +0530

    First commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,10 +4,11 @@ const Contact = () => {
     return (
         <div className="social-links">
         <p>CONTACT</p>
-		<i className="fa fa-facebook" ></i>
-		<i className="fa fa-instagram" ></i>
-		<i className="fa fa-twitter" ></i>
-		<i class="fa fa-youtube-play" ></i>
+		<a href="https://github.com/Achyuthc"><i class="fab fa-github"></i></a>
+		<a href="https://www.linkedin.com/in/achyuth-chowdary-d-a-5629b7211"><i class="fab fa-linkedin-in"></i></a>
+		
+		<a href="https://www.hackerrank.com/achyuthchowdary3"><i class="fab fa-hackerrank"></i></a>
+		<a href="https://valueinvestingvi.blogspot.com/2021/06/value-investing-and-assets.html?m=1" > <i class="fab fa-blogger"></i> </a>
 		<p>Copyright Achyuth Chowdary D A</p>
 		<p>achyuthchowdary30@gmail.com</p>
 	</div>
